f0ddbc358422d500a22fc00f5092873c4e4d701c
Stef Colruyt <stef.col@hotmail.com>
2017-01-21 00:34:49 +0100

    Added symple physics movement

--- SumoDucks_Unity/Assets/Duck/Movement.cs
+++ SumoDucks_Unity/Assets/Duck/Movement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    [Header("General Info")]
+    public int playerNum;
+
+    public float movSpeed;
+    public Rigidbody rb;
+
+    [Header("Movement")]
+    private bool x;
+    public bool forward, backward, left, right, a, b;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        rb.velocity = new Vector3(Input.GetAxis("Player_" + playerNum + "_Horizontal"), 0, Input.GetAxis("Player_" + playerNum + "_Vertical"));
+    }
 }
