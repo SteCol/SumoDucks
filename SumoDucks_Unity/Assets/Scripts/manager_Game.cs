e14c702804685ef333628a8e82c66e935369f179
Anton Shumin <AntonShuminBe@gmail.com>
2017-01-21 01:17:45 +0100

    prep game manager script

--- SumoDucks_Unity/Assets/Scripts/manager_Game.cs
+++ SumoDucks_Unity/Assets/Scripts/manager_Game.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class manager_Game : MonoBehaviour {
 
+    public bool game_in_progress = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        round_begin();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void round_begin()
+    {
+        game_in_progress = true;
+    }
+
+    public void exit_ring()
+    {
+        game_in_progress = false;
+
+    }
+
 }
