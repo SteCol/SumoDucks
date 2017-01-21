cc8ea51b8e0483c02d054b1237ab5ac815c0e984
Michiel Van Dyck <michiel.vandyck@uantwerpen.be>
2017-01-21 15:15:50 +0100

    mexican wave

--- SumoDucks_Unity/Assets/Scripts/water_script.cs
+++ SumoDucks_Unity/Assets/Scripts/water_script.cs
@@ -22,15 +22,19 @@ public class water_script : MonoBehaviour {
     public int height = 1;
     public float xSize, ySize = 1;
     public float eventTime;
-    public float waveSize=1;
+    public float eventTimes[];
+    public float waveSize = 1f;
+
     Vector3 eventPos;
     // Use this for initialization
     void Awake () {
         mesh = GetComponent<MeshFilter>().mesh = ParametricPlane.GeneratePlane(width, height, xSize / 2, ySize / 2, 0, xSize, ySize);
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        waveSize = 1f;
+
+}
+
+// Update is called once per frame
+void Update () {
         
         if (baseHeight == null)
             baseHeight = mesh.vertices;
@@ -44,8 +48,8 @@ public class water_script : MonoBehaviour {
             //   vertex.y += (Mathf.Sin(Time.time * -speed + 2f * distance1 + baseHeight[i].y * 0 ) * scale) *1 / (distance1 + 1f);
 
             //vertex.y += (Mathf.Sin((Time.time-eventTime) * -speed + 2f * (distance1- (Time.time - eventTime)) + baseHeight[i].y * 0) * scale) * 1 / (distance1 + 1f);
-            float wavecentre = Mathf.Pow((((Time.time - eventTime) * speed) - distance1), 2);
-            vertex.y = Mathf.Cos(wavecentre)*(((wavecentre < 3.14 ) ? 3.14f:0)-0) * waveSize ;
+            float wavecentre = Mathf.Abs(((Time.time - eventTime) * speed) - distance1);
+            vertex.y = Mathf.Cos(wavecentre*3.14f)*(((wavecentre < 1 ) ? (1f-wavecentre): 0)- 0) * waveSize ;
 
             float distance2 = Mathf.Sqrt(Mathf.Pow(baseHeight[i].x - srcx2, 2) + Mathf.Pow(baseHeight[i].z - srcy2, 2));
           //  vertex.y += Mathf.Sin(Time.time * -speed + 3f*distance2 + baseHeight[i].y * 0) * scale *1/ (distance2 + 1f);
