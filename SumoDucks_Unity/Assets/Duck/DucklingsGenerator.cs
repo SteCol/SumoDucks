64acd60b3fc33e318d722a800a763f6d225a6a67
Stef Colruyt <stef.col@hotmail.com>
2017-01-21 03:02:52 +0100

    Added ducklng spawner + start wave system

---
+++ SumoDucks_Unity/Assets/Duck/DucklingsGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DucklingsGenerator : MonoBehaviour {
+    public GameObject ducklingContainer;
+
+    public int desiredAmount, currentAmount;
+
+    public GameObject ducklingPrefab;
+    public List<GameObject> ducklings;
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (desiredAmount != currentAmount) {
+            ducklings.Clear();
+
+            foreach (GameObject d in GameObject.FindGameObjectsWithTag("Duckling"))
+            {
+                Destroy(d);
+            }
+
+            for (int d = 0; d < desiredAmount; d++)
+            {
+                GameObject duckling = Instantiate(ducklingPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                ducklings.Add(duckling);
+                duckling.transform.parent = ducklingContainer.transform;
+            }
+
+            for (int d = 0; d < ducklings.Count; d++)
+            {
+                if (d - 1 >= 0)
+                    ducklings[d].GetComponent<FollowScript>().toFollow = ducklings[d - 1];
+                else
+                    ducklings[d].GetComponent<FollowScript>().toFollow = this.gameObject;
+            }
+
+            currentAmount = desiredAmount;
+        }
+	}
+}
