49b82d57492c81e01c21188114641aad192332e0
Stef Colruyt <stef.col@hotmail.com>
2017-01-21 13:16:05 +0100

    Made playering pub & added explosionforce

--- SumoDucks_Unity/Assets/Duck/Projectile.cs
+++ SumoDucks_Unity/Assets/Duck/Projectile.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
     private Rigidbody rb;
+    public float scaleSpeed;
     public float scale;
     public float lifeTime;
     public GameObject generatedFrom;
@@ -12,31 +14,38 @@ public class Projectile : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //rb = this.GetComponent<Rigidbody>();
         Destroy(this.gameObject, lifeTime);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.Translate(-Vector3.forward * speed * Time.deltaTime);
 
         //rb.AddRelativeForce(-Vector3.forward * speed);
-        scale = scale + 10 * Time.deltaTime;
+        scale = scale + scaleSpeed * Time.deltaTime;
         transform.localScale = new Vector3(scale, scale, scale);
     }
 
     void OnTriggerStay(Collider c)
     {
-        if (c.GetComponent<Rigidbody>() != null) {
-            c.GetComponent<Rigidbody>().AddExplosionForce(force, this.transform.position, scale);
-        }
-        //Push away from center
-        /*
-            Vector3 dir = c.transform.position - transform.position;
-            dir = -dir.normalized;
-            c.GetComponent<Rigidbody>().AddForce(-dir * force);
-            */
         
+        if (c.GetComponent<Rigidbody>() != null) {
+            c.GetComponent<Rigidbody>().AddExplosionForce(force * 50, this.transform.position, scale);
+        }
+
+        Destroy(this.gameObject);
+        //Push away from center
+
+        /*
+        Vector3 dir = c.transform.position - transform.position;
+        dir = -dir.normalized;
+        c.GetComponent<Rigidbody>().AddForce(-dir * force);
+        */
+
+
     }
 }
