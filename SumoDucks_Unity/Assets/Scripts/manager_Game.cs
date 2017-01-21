5f3527b4107ada205bc62b2782b273af3a6e44f6
Anton Shumin <AntonShuminBe@gmail.com>
2017-01-21 15:01:21 +0100

    update start scene

--- SumoDucks_Unity/Assets/Scripts/manager_Game.cs
+++ SumoDucks_Unity/Assets/Scripts/manager_Game.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class manager_Game : MonoBehaviour {
 
-    public bool game_in_progress = false;
+    public bool game_in_progress = true;
     public GameObject prefab_duck;
     public GameObject[] m_spown_points = new GameObject[2];
     public manager_UI m_manager_ui;
@@ -33,13 +33,6 @@ public class manager_Game : MonoBehaviour {
     public void round_begin()
     {
 
-        //clear old ducks
-        foreach (GameObject duck in ducks)
-        {
-            Destroy(duck);
-        }
-        ducks.Clear();
-
         //hide welcome ui
         GameObject welcome_text = m_manager_ui.m_elements_ui[0];
         welcome_text.SetActive(false);
@@ -54,9 +47,12 @@ public class manager_Game : MonoBehaviour {
             //set duck properties
             GameObject duck_child = new_duck.GetComponent<ParentDuck>().children[1];
             duck_child.GetComponent<Movement>().playerNum = i + 1;
+            ducks.Add(duck_child);
             
 
         }
+
+        game_in_progress = true;
 
 
         
@@ -71,10 +67,25 @@ public class manager_Game : MonoBehaviour {
     public void reset_round()
     {
         
+        if(game_in_progress)
+        {
+            //clear old ducks
+            foreach (GameObject duck in ducks)
+            {
+                Destroy(duck);
+            }
+            ducks.Clear();
 
-        //get welcome text
-        GameObject welcome_text = m_manager_ui.m_elements_ui[0];
-        welcome_text.SetActive(true);
+            //get welcome text
+            GameObject welcome_text = m_manager_ui.m_elements_ui[0];
+            welcome_text.SetActive(true);
+
+        } else
+        {
+            round_begin();
+        }
+
+        
 
 
     }
